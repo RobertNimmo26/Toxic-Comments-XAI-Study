84a46d1e5d7c0d3b2b72bf9329d1d426414ded67
Robert Nimmo <robertnimmo26@live.co.uk>
2022-11-28 17:09:36 +0000

    Changes text explanation if browser screen is too small

--- frontend/src/pages/ScreenSmall.js
+++ frontend/src/pages/ScreenSmall.js
@@ -4,13 +4,12 @@ const ScreenSize = () => {
   return (
     <>
       <Container>
-        <h1 styles={{ textAlign: "center" }}>
-          Your screen width is too small for this study.
-        </h1>
+        <h1>Your screen width is too small for this study.</h1>
         <h5>
           Please make sure you are taking this study on a device with a large
-          screen (desktop/laptop/tablet). If you are completing the study on a
-          device in portrait mode, turn your device around to landscape.
+          screen (desktop/laptop). If you are completing the study on a device
+          in portrait mode or on a small browser window, turn your device around
+          to landscape or resize your browser window.
         </h5>
       </Container>
     </>
