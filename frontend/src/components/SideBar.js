57cca6f0f71480b1bcdfe9fd3c5ec4fe782921a9
Robert Nimmo <robertnimmo26@live.co.uk>
2023-01-23 01:59:11 +0000

    Added comment label to comment list

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -13,7 +13,7 @@ const SideBar = () => {
     <>
       <ListGroup variant="flush" style={{ height: "100vh", overflowY: "auto" }}>
         {explanationData.user.map((x, i) => (
-          <ListGroup.Item action eventKey={i + 1} key={i + 1}>
+          <ListGroup.Item action eventKey={i + 1}>
             <div className="d-flex w-100 align-items-center justify-content-between">
               <strong className="mb-1">Comment {i + 1}</strong>
               {explanationData.user[i].checked ? (
@@ -22,8 +22,13 @@ const SideBar = () => {
                 <small>Unchecked</small>
               )}
             </div>
-            <div className="col-10 mb-1 small">
-              {x.comment.split(" ").slice(0, 5).join(" ")}
+            <div className="col-10 mb-1 small d-flex w-100 justify-content-between">
+              {x.comment.split(" ").slice(0, 5).join(" ")}...
+              {explanationData.user[i].prediction_label === "Toxic" ? (
+                <strong style={{ textAlign: "right" }}>Toxic</strong>
+              ) : (
+                <strong style={{ textAlign: "right" }}>Non-toxic</strong>
+              )}
             </div>
           </ListGroup.Item>
         ))}
