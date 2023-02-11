e138d3b1919eaaea919fe3f2ff003e7f2621c391
Robert Nimmo <robertnimmo26@live.co.uk>
2023-02-11 21:23:45 +0000

    Updated actual understanding questionnaire

---
+++ frontend/src/components/TextAreaQuestionare.js
@@ -0,0 +1,35 @@
+// import react components
+import { useContext } from "react";
+
+// import bootstrap components
+import Form from "react-bootstrap/Form";
+import TextareaCounter from "react-textarea-counter";
+
+const TextAreaQuestionare = ({ questionData, questionContext }) => {
+  const { postTaskForm, setPostTaskForm } = useContext(questionContext);
+
+  const handleChange = (event) => {
+    setPostTaskForm({
+      ...postTaskForm,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <>
+      <Form.Group controlId={questionData.id}>
+        <Form.Label>{questionData.question}</Form.Label>
+        <TextareaCounter
+          countLimit={1000}
+          onChange={handleChange}
+          rows={4}
+          class="form-control"
+          id={questionData.id}
+        />
+      </Form.Group>
+      <br />
+    </>
+  );
+};
+
+export default TextAreaQuestionare;
